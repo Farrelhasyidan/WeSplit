592eb7da92372716e4d749f6de1b768f2f7f5649
Farrelhasyidan <farrelhasyidan@gmail.com>
2021-04-06 20:15:12 +0700

    we split money

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  
+  @State private var CheckAmmont = ""
+  @State private var numberOfPeople = 2
+  @State private var tipPercentage = 2
+  
+  let tipPercentages = [10, 15, 20, 25, 0]
+  
+  var totalPerPerson: Double{
+    let peopleCount = Double(numberOfPeople + 2)
+    let tipsSelection = Double(tipPercentages[tipPercentage])
+    let orderAmount = Double(CheckAmmont) ?? 0
+    
+    let tipValue = orderAmount / 100 * tipsSelection
+    let grandTotal = orderAmount + tipValue
+    let AmountPerPerson = grandTotal / peopleCount
+    
+    return AmountPerPerson
+  }
+  
+  var body: some View{
+    NavigationView{
+      
+      Form{
+        Section {
+          TextField("Amount", text: $CheckAmmont)
+          Picker("Number of people", selection: $numberOfPeople){
+            ForEach(2..<20){ number in
+              Text("\(number) people")
+            }
+          }
+        }
+        
+        Section(header: Text("How muct tip do you want to leave")){
+          Picker("Tip Percentage", selection: $tipPercentage){
+            ForEach(0..<tipPercentages.count) {
+              Text("\(self.tipPercentages[$0])%")
+            }
+          }
+          .pickerStyle(SegmentedPickerStyle())
+        }
+        
+        Section{
+          Text("$\(totalPerPerson, specifier: "%.2f")")
+        }
+      }
+      .navigationBarTitle("We Split")
     }
+  }
 }
 
+
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
---
+++ WeSplit/WeSplit.swift
@@ -0,0 +1,20 @@
+//
+//  WeSplit.swift
+//  WeSplit
+//
+//  Created by Farrel hasyidan on 06/04/21.
+//
+
+import SwiftUI
+
+struct WeSplit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeSplit_Previews: PreviewProvider {
+    static var previews: some View {
+        WeSplit()
+    }
+}
